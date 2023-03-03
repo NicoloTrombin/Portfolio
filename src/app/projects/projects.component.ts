227fcda6fc829a189b094b6a4aa84df6d6c97597
Nicolò Trombin <nikitrombin744@gmail.com>
2023-03-03 15:16:13 +0100

    Modified projects images display

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,97 +7,78 @@ import { Component, Inject, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
-  //  slideIndex = 1;
+    slideIndex = 1;
+    slideIndex1 = 1;
+    slideIndex2 = 1;
 
-  //   constructor(@Inject(DOCUMENT) private document: Document) {}
-
-  //  ngOnInit(): void {
-  //    this.showSlides(this.slideIndex);
-  //   }
-
-  //   // Next/previous controls
-  //  plusSlides(n : number) {
-  //     this.showSlides(this.slideIndex += n);
-  //   }
-
-  //   // Thumbnail image controls
-  //   currentSlide(n : number) {
-  //     this.showSlides(this.slideIndex = n);
-  //   }
-
-  //   showSlides(n : number) {
-  //     let i;
-  //     let slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
-  //     let dots = document.getElementsByClassName("dot");
-  //     if (n > slides.length) {this.slideIndex = 1}
-    
-  //     if (n < 1) {this.slideIndex = slides.length}
-  //     for (i = 0; i < slides.length; i++) {
-  //       slides[i].style.display = "none";
-  //     }
-  //     for (i = 0; i < dots.length; i++) {
-  //       dots[i].className = dots[i].className.replace(" active", "");
-  //     }
-  //      slides[this.slideIndex-1].style.display = "block";
-  //     dots[this.slideIndex-1].className += " active";
-  //   }
-
-     slideIndex = 0;
-     slideIndex1 = 0;
-     slideIndex2 = 0;
-     slideIndex3 = [1, 1, 1];
-
-     ngOnInit() : void {
-      this.showSlides();  
-      this.showSlides1();
-      this.showSlides2();
-    }
-
-    showSlides() {
-     let i;
-     let slides : any  = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
-
-     for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";
+    ngOnInit(): void {
+      this.showSlides(this.slideIndex);
+      this.showSlides1(this.slideIndex1);
+      this.showSlides2(this.slideIndex2);
      }
 
-     this.slideIndex++;
-    
-     if (this.slideIndex > slides.length) {this.slideIndex = 1}
-    
-     slides[this.slideIndex-1].style.display = "block";
-     setTimeout(() => this.showSlides(), 4000);
-   }
+     // Thumbnail image controls
+     currentSlide(n : number) {
+       this.showSlides(this.slideIndex = n);
+     }
 
-   showSlides1() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides1") as HTMLCollectionOf<HTMLElement>;
-
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+     currentSlide1(n1 : number) {
+      this.showSlides1(this.slideIndex1 = n1);
     }
 
-    this.slideIndex1++;
-   
-    if (this.slideIndex1 > slides.length) {this.slideIndex1 = 1}
-   
-    slides[this.slideIndex1-1].style.display = "block";
-    setTimeout(() => this.showSlides1(), 4000);
-  }
-
-  showSlides2() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides2") as HTMLCollectionOf<HTMLElement>;
-
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    currentSlide2(n2 : number) {
+      this.showSlides2(this.slideIndex2 = n2);
     }
 
-    this.slideIndex2++;
+     showSlides(n : number) {
+       let i;
+       let slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
+       let dots = document.getElementsByClassName("dot") as HTMLCollectionOf<HTMLElement>;
+       if (n > slides.length) {this.slideIndex = 1}
+    
+       if (n < 1) {this.slideIndex = slides.length}
+       for (i = 0; i < slides.length; i++) {
+         slides[i].style.display = "none";
+       }
+       for (i = 0; i < dots.length; i++) {
+         dots[i].className = dots[i].className.replace(" active", "");
+       }
+        slides[this.slideIndex-1].style.display = "block";
+        dots[this.slideIndex-1].className += " active";
+     }
+
+     showSlides1(n : number) {
+      let i;
+      let slides = document.getElementsByClassName("mySlides1") as HTMLCollectionOf<HTMLElement>;
+      let dots = document.getElementsByClassName("dot1") as HTMLCollectionOf<HTMLElement>;
+      if (n > slides.length) {this.slideIndex1 = 1}
    
-    if (this.slideIndex2 > slides.length) {this.slideIndex2 = 1}
+      if (n < 1) {this.slideIndex1 = slides.length}
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+       slides[this.slideIndex1-1].style.display = "block";
+       dots[this.slideIndex1-1].className += " active";
+    }
+
+    showSlides2(n : number) {
+      let i;
+      let slides = document.getElementsByClassName("mySlides2") as HTMLCollectionOf<HTMLElement>;
+      let dots = document.getElementsByClassName("dot2") as HTMLCollectionOf<HTMLElement>;
+      if (n > slides.length) {this.slideIndex2 = 1}
    
-    slides[this.slideIndex2-1].style.display = "block";
-    setTimeout(() => this.showSlides2(), 4000);
-  }
+      if (n < 1) {this.slideIndex2 = slides.length}
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+       slides[this.slideIndex2-1].style.display = "block";
+       dots[this.slideIndex2-1].className += " active";
+    }
+
 }
